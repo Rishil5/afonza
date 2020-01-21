f2ecc60c46da4f4ac128081705369e3a001f163b
Rishil Shaji <rishil.shaji@crozaint.com>
2020-01-22 02:38:36 +0530

    first commit

---
+++ publish/wwwroot/js/navigation.js
@@ -0,0 +1,23 @@
+﻿// Navigation ACTIVE class
+$(document).ready(function () {
+    $('.nav-item a[href="' + document.location.pathname + '"]').parent().addClass('active');
+});
+
+$(document).ready(function () {
+    if (document.location.pathname == '/' || document.location.pathname == '') {
+        $('.nav-item a[href="/"]').parent().addClass('active');
+    }
+});
+
+$(document).ready(function () {
+    $('.dropdown-menu a[href="' + document.location.pathname + '"]').addClass('active');
+
+    if ($('.dropdown-item').hasClass("active"))
+    {
+        $('.nav-item a[href="#"]').parent().addClass('active');
+    }
+});
+
+$(document).ready(function () {
+    $('.navbar-nav a[href="' + document.location.pathname + '"]').addClass('active');
+});
